2b049a9c0e97d864a36c300d19a4e13441987904
Aniyikaye-Alabi <66259809+Aniyikaye-Alabi@users.noreply.github.com>
2022-10-30 03:09:16 +0000

    Update index.php

--- index.php
+++ index.php
@@ -50,36 +50,36 @@
     );
   }
 
-  ?>
-=======
-<?php
-    session_start();
-    include("connection_string.php");
-
-    //$id = $_SESSION['id'];
-    $sql = "SELECT * FROM `slackbio` LIMIT 1";
-    $result = mysqli_query($conn, $sql);
-    // $row = mysqli_fetch_assoc($result);
-
-    // $bioJson = json_encode($row);
-
-    // echo $bioJson;
-
-
-    if (mysqli_num_rows($result) > 0) {
-        while($row = mysqli_fetch_assoc($result)) {
-            $arr = array(
-                "slackUsername" => $row['slackUsername'],
-                "backend" => ($row['backend'] == 1) ? true : false,
-                "age" => (int)$row['age'],
-                "bio" => $row['bio']
-            );
-
-            $bioJson = json_encode($arr);
-            echo $bioJson;
-        }
-
-    } else {
-    //     echo "0 results";
-    }
 ?>
+
+<!-- <?php -->
+//     session_start();
+//     include("connection_string.php");
+
+//     //$id = $_SESSION['id'];
+//     $sql = "SELECT * FROM `slackbio` LIMIT 1";
+//     $result = mysqli_query($conn, $sql);
+//     // $row = mysqli_fetch_assoc($result);
+
+//     // $bioJson = json_encode($row);
+
+//     // echo $bioJson;
+
+
+//     if (mysqli_num_rows($result) > 0) {
+//         while($row = mysqli_fetch_assoc($result)) {
+//             $arr = array(
+//                 "slackUsername" => $row['slackUsername'],
+//                 "backend" => ($row['backend'] == 1) ? true : false,
+//                 "age" => (int)$row['age'],
+//                 "bio" => $row['bio']
+//             );
+
+//             $bioJson = json_encode($arr);
+//             echo $bioJson;
+//         }
+
+//     } else {
+//     //     echo "0 results";
+//     }
+// ?>
