c4818a33e8cba007caae298b21679f0fc01d9190
Aniyikaye Alabi <adalabi@sidmach.com>
2022-10-30 01:48:43 +0100

    Initial Release

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+// $name = "";
+// $color = "";
+// $food = ;
+
+// $arr = array(
+//     $name => 'Great',
+//     $color => 'Green',
+//     $food => 'Ramen',
+// );
+
+// $obj = json_encode($arr);
+// echo $obj;
+
+$age = array("Peter"=>35, "Ben"=>37, "Joe"=>43);
+
+echo json_encode($age);
+
+?>
